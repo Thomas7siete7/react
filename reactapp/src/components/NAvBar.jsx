ecc485d1cacaed23d6ae90f4c3b25722a4a4a0fc
Thomas Marin <thomas7siete7@gmail.com>
2022-05-28 13:12:18 -0300

    Agregado de cosas

---
+++ reactapp/src/components/NAvBar.jsx
@@ -0,0 +1,18 @@
+const NAvBar=()=>{
+    return (
+        <div>
+            <header>
+                <nav>
+                    <a href="index.html">Comprax</a>
+                    <lu>
+                        <li>Perfil</li>
+                        <li>Ofertas</li>
+                        <li>Configuracion</li>
+                    </lu>
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+export default NAvBar;
