ff1a9f56d57d7bae72dd5a338057077a6c79e72c
Thomas Marin <thomas7siete7@gmail.com>
2022-06-01 23:02:22 -0300

    completado de ejercicio

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,27 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Counter from './components/NAvBar';
 import NAvBar from './components/NAvBar';
+import saludar from './ItemListContainer/listContainer';
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */
-        <NAvBar></NAvBar>
-      }
+      
+      <NAvBar/>
+      <saludar mensaje={'buenonas'}/>
+      
     </div>
   );
 }
---
+++ reactapp/src/ItemListContainer/listContainer.jsx
@@ -0,0 +1,8 @@
+const saludar=(param)=>{
+    return(
+        <h1 className="item">{param.mensaje}</h1>
+
+    )
+}
+
+export default saludar;
