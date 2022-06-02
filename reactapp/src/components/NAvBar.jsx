54099372b1b021f15b95f4447d87092562bdeb76
Thomas Marin <thomas7siete7@gmail.com>
2022-06-01 22:27:42 -0300

    Agregado de estilos

--- reactapp/src/components/NAvBar.jsx
+++ reactapp/src/components/NAvBar.jsx
@@ -1,14 +1,24 @@
 const NAvBar=()=>{
     return (
         <div>
-            <header>
-                <nav>
-                    <a href="index.html">Comprax</a>
-                    <lu>
-                        <li>Perfil</li>
-                        <li>Ofertas</li>
-                        <li>Configuracion</li>
-                    </lu>
+            <header className="header">
+                <nav className="flex ">
+                    <a className="letras-nav" href="index.html">Comprax</a>
+                    <ul className="flex ul letras-nav">
+                        <li className="container-menu"><a href="#" className="letras-nav ">Perfil</a>
+                            <ul className="subMenu">
+                                <li className="item">Ver</li>
+                                <li className="item">Cambiar</li>
+                            </ul>
+                        </li>
+                        <li className="container-menu"><a href="#" className="letras-nav">Ofertas</a>
+                            <ul className="subMenu">
+                                <li>Muebles</li>
+                                <li>Electrodomesticos</li>
+                            </ul>
+                        </li>
+                        <li><a href="#" className="letras-nav">Configuracion</a></li>
+                    </ul>
                 </nav>
             </header>
         </div>
