cf08f5c76d0ecf4b39a4c85f782fbd6ede47491a
Thomas Marin <thomas7siete7@gmail.com>
2022-06-04 19:10:47 -0300

    Agregadosss

--- reactapp/src/ItemListContainer/listContainer.jsx
+++ reactapp/src/ItemListContainer/listContainer.jsx
@@ -1,6 +1,25 @@
+import { useState, useEffect } from "react";
+import { getCartas } from "../components/async";
+
+
 const Saludar=(param)=>{
+    const[cartas, setCartas] = useState([])
+    useEffect( () => {
+        getCartas().then(response=>{
+            setCartas(response)
+        })
+    }, [])
+    
     return(
-        <h1 className="item">{param.mensaje}</h1>
+        
+        <div>
+            <h1 className="item">{param.mensaje}</h1>
+
+            <ul>
+                {cartas.map(carta=> <li key={carta.id}>{carta.numero}{' '}{carta.palo}</li>)}
+            </ul>
+        </div>
+        
 
     )
 }
