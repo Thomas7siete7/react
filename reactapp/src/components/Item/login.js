67dc3de4a0078fc7a86db8e8a460e345138f1c56
Thomas7siete7 <thomas7siete7@gmail.com>
2022-07-15 16:13:30 -0300

    agregado de funcionalidad al form

--- reactapp/src/components/Item/login.js
+++ reactapp/src/components/Item/login.js
@@ -1,24 +1,22 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Logear =()=>{
+    const [nombre, setNombre]= useState('')
+    const [contraseña, setContraseña]= useState('')
+    const [direccion, setDireccion]= useState('')
+    const [telefono, setTelefono]= useState('')
 
-    const info=()=>{
-        const informacion={
-
-        }
-    }
-
-    const capturar
 
     return (
         <div>
 
             <form>
                 <h2>Registrarse</h2>
-                <input type="text" placeholder="Correo" name="correo"></input>
-                <input type="password" placeholder="Contraseña" name="contraseña"></input>
-                <input type="text" placeholder="Direccion" name="direccion"></input>
-                <input type="number" placeholder="Telefono" name="telefono"></input>
+                <input type="text" placeholder="Correo" name="correo" value={nombre} onChange={(e)=> setNombre(e.target.value)}></input>
+                <input type="password" placeholder="Contraseña" name="contraseña" value={contraseña} onChange={(e)=> setContraseña(e.target.value)}></input>
+                <input type="text" placeholder="Direccion" name="direccion" value={direccion} onChange={(e)=> setDireccion(e.target.value)}></input>
+                <input type="number" placeholder="Telefono" name="telefono" value={telefono} onChange={(e)=> setTelefono(e.target.value)}></input>
 
                 <Link to='/'>
                         <h3>Comprar</h3>
